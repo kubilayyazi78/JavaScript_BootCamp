1e0eec68dcce24c1c8626b797ca0c4fa9440238d
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-10-23 18:01:20 +0300

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,1 +1,23 @@
 console.log("Kodlama IO")
+
+var  dolarDun=9.20
+let  dolarBugun=9.30
+{
+    var dolarDun=9.10
+}
+
+console.log(dolarDun)
+
+const euroDun=11.2
+
+//euroDun=11
+
+console.log(euroDun)
+
+let konutKredileri=["Konut","Emlak Konut","Kamu"]
+console.log("<ul>")
+for(let i =0; i<konutKredileri.length;i++){
+  console.log("<li>"+konutKredileri[i]+"</li>")
+}
+console.log("</ul>")
+console.log(konutKredileri);
