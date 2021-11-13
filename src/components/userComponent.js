9ec4e8401c52a64bd23738e50fdd95543e280fdd
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-11-13 20:23:57 +0300

    Loglama simülasyonu yapıldı.

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,9 +1,12 @@
+import { BaseLogger, ElasticLogger } from "../crossCuttingConcerns/logging/logger.js";
 import User from "../models/user.js";
 import UserService from "../services/userService.js"
 
 console.log("UserComponent Loaded")
 
-let userService=new UserService()
+
+let logger1=new ElasticLogger()
+let userService=new UserService(logger1)
 
 let user1=new User(1,"Kubilay","Yazı","Karabük")
 let user2=new User(2,"Alp","Hnc","İst")
